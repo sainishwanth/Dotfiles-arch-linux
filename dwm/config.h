7da5264605775b515700413d1e3ed3ec20e2f04a
Sai Nishwanth <sainishwanthraj@gmail.com>
2023-01-07 22:28:37 +0530

    new st and dwm build

--- dwm/config.h
+++ dwm/config.h
@@ -8,27 +8,27 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih = 10; // Inner Horizontal
 static const unsigned int gappiv = 10; // Inner Vertical
 static const unsigned int gappoh = 15; //Outer Horizontal
-static const unsigned int gappov = 20; // Outer Vertical
+static const unsigned int gappov = 15; // Outer Vertical
 static const int usealtbar = 0;
 static const char *altbarclass = "Polybar";
 static const char *altbarcmd = "$HOME/bar.sh";
 static const int smartgaps = 0;
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono-NL:size=12" };
-static const char dmenufont[]       = "JetBrainsMono-NL:size=12";
-static const char col_gray1[]       = "#222222";
+static const char *fonts[]          = { "mono:size=12" };
+static const char dmenufont[]       = "mono:size=12";
+static const char col_gray1[]       = "#6d2825";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#90EE90"; //dwm status bar
+static const char col_cyan[]        = "#392c25"; //dwm status bar
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -67,7 +67,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
+//static const char *kittycmd[]  = { "kitty", NULL };
+static const char *termcmd[]  = { "st", NULL };
 //static const char *roficmd[]  = { "rofi", "-show", "drun", "-show-icons", NULL};
 static const char *roficmd[]  = { "custom_rofi", NULL};
 static const char *escrotum[] = { "escrotum", "-s", "-C", NULL};
@@ -77,13 +78,14 @@ static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%"
 static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
 static Key keys[] = {	
 	/* modifier                     key        function        argument */
-    { MODKEY,                       XK_F11,    spawn,          {.v = downvol } },
+    { MODKEY,                     XK_F11,    spawn,          {.v = downvol } },
 	{ MODKEY,                       XK_F10,    spawn,          {.v = mutevol } },
 	{ MODKEY,                       XK_F12,    spawn,          {.v = upvol   } },
-	{ MODKEY,			            XK_s,	   spawn,	       {.v = escrotum} },
-	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
+	{ MODKEY,			                  XK_s,	     spawn,	         {.v = escrotum} },
+  { MODKEY|ShiftMask,             XK_S,      spawn,          {.v = escrotum_full} },
+  { MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
 	{ MODKEY,                       XK_o,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -93,7 +95,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -72,17 +72,17 @@ nvim_lsp.flow.setup {
   capabilities = capabilities
 }
 
---nvim_lsp.tsserver.setup {
---  on_attach = on_attach,
---  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
---  cmd = { "typescript-language-server", "--stdio" },
---  capabilities = capabilities
---}
+nvim_lsp.tsserver.setup {
+  on_attach = on_attach,
+  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+  cmd = { "typescript-language-server", "--stdio" },
+  capabilities = capabilities
+}
 
---nvim_lsp.sourcekit.setup {
---  on_attach = on_attach,
---  capabilities = capabilities,
---}
+nvim_lsp.sourcekit.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
 
 nvim_lsp.sumneko_lua.setup {
   capabilities = capabilities,
@@ -106,20 +106,20 @@ nvim_lsp.sumneko_lua.setup {
   },
 }
 
---nvim_lsp.tailwindcss.setup {
---  on_attach = on_attach,
---  capabilities = capabilities
---}
+nvim_lsp.tailwindcss.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
 
---nvim_lsp.cssls.setup {
---  on_attach = on_attach,
---  capabilities = capabilities
---}
---
---nvim_lsp.astro.setup {
---  on_attach = on_attach,
---  capabilities = capabilities
---}
+nvim_lsp.cssls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+
+nvim_lsp.astro.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
